c6439b4174a151429f9f3f69c2beb4fc1ac96bb7
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-03-27 23:56:04 +0700

    feat: add food manage

--- src/main/java/com/quangta/config/JwtConstant.java
+++ src/main/java/com/quangta/config/JwtConstant.java
@@ -4,5 +4,5 @@ public class JwtConstant {
 
     public static final String SECRET_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
-    public static final String JWT_Header = "Authorization";
+    public static final String JWT_HEADER = "Authorization";
 }
---
+++ src/main/java/com/quangta/controller/FoodController.java
@@ -0,0 +1,55 @@
+package com.quangta.controller;
+
+import com.quangta.entity.Food;
+import com.quangta.entity.Restaurant;
+import com.quangta.entity.User;
+import com.quangta.payload.CreateFoodRequest;
+import com.quangta.service.FoodService;
+import com.quangta.service.RestaurantService;
+import com.quangta.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/food")
+public class FoodController {
+
+    private final FoodService foodService;
+
+    private final UserService userService;
+
+    private final RestaurantService restaurantService;
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Food>> searchFood(
+            @RequestParam String name,
+            @RequestHeader("Authorization") String jwtToken
+    ) throws Exception {
+        User user = userService.findUserByJwtToken(jwtToken);
+
+        List<Food> foods = foodService.searchFood(name);
+
+        return new ResponseEntity<>(foods, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/restaurant/{restaurantId}")
+    public ResponseEntity<List<Food>> getRestaurantFood(
+            @RequestParam boolean isVegetarian,
+            @RequestParam boolean isSeasonal,
+            @RequestParam boolean isNonVegetarian,
+            @RequestParam(required = false) String food_category,
+            @PathVariable Long restaurantId,
+            @RequestHeader("Authorization") String jwtToken
+    ) throws Exception {
+        User user = userService.findUserByJwtToken(jwtToken);
+
+        List<Food> foods = foodService.getRestaurantFood(restaurantId, isVegetarian, isNonVegetarian, isSeasonal, food_category);
+
+        return new ResponseEntity<>(foods, HttpStatus.OK);
+    }
+}
