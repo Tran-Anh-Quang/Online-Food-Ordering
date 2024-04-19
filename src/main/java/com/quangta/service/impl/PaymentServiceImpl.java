5f97b105c1ab3852099dd5367de88737e9bbff97
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-04-19 18:05:09 +0700

    chore: test integrate sonarqube

--- src/main/java/com/quangta/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/quangta/service/impl/PaymentServiceImpl.java
@@ -1,54 +1,54 @@
-package com.quangta.service.impl;
-
-import com.quangta.entity.Order;
-import com.quangta.payload.response.PaymentResponse;
-import com.quangta.service.PaymentService;
-import com.stripe.Stripe;
-import com.stripe.exception.StripeException;
-import com.stripe.model.checkout.Session;
-import com.stripe.param.checkout.SessionCreateParams;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-@Service
-public class PaymentServiceImpl implements PaymentService {
-    @Value("${stripe.api.key}")
-    private String stripeSecretKey;
-
-    @Override
-    public PaymentResponse createPaymentLink(Order order) throws StripeException {
-
-        Stripe.apiKey = stripeSecretKey;
-
-        SessionCreateParams params = SessionCreateParams
-                .builder()
-                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
-                .setMode(SessionCreateParams.Mode.PAYMENT)
-                .setSuccessUrl("http://localhost:3000/payment/success/" + order.getId())
-                .setCancelUrl("http://localhost:3000/payment/fail")
-                .addLineItem(
-                        SessionCreateParams.LineItem.builder()
-                                .setQuantity(1L)
-                                .setPriceData(
-                                        SessionCreateParams.LineItem.PriceData.builder()
-                                                .setCurrency("usd")
-                                                .setUnitAmount(order.getTotalPrice() * 100)
-                                                .setProductData(
-                                                        SessionCreateParams.LineItem.PriceData.ProductData.builder()
-                                                                .setName("Test")
-                                                                .build()
-                                                )
-                                                .build()
-                                )
-                                .build()
-                )
-                .build();
-
-        Session session = Session.create(params);
-
-        PaymentResponse paymentResponse = new PaymentResponse();
-        paymentResponse.setPayment_url(session.getUrl());
-
-        return paymentResponse;
-    }
-}
+//package com.quangta.service.impl;
+//
+//import com.quangta.entity.Order;
+//import com.quangta.payload.response.PaymentResponse;
+//import com.quangta.service.PaymentService;
+//import com.stripe.Stripe;
+//import com.stripe.exception.StripeException;
+//import com.stripe.model.checkout.Session;
+//import com.stripe.param.checkout.SessionCreateParams;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class PaymentServiceImpl implements PaymentService {
+//    @Value("${stripe.api.key}")
+//    private String stripeSecretKey;
+//
+//    @Override
+//    public PaymentResponse createPaymentLink(Order order) throws StripeException {
+//
+//        Stripe.apiKey = stripeSecretKey;
+//
+//        SessionCreateParams params = SessionCreateParams
+//                .builder()
+//                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+//                .setMode(SessionCreateParams.Mode.PAYMENT)
+//                .setSuccessUrl("http://localhost:3000/payment/success/" + order.getId())
+//                .setCancelUrl("http://localhost:3000/payment/fail")
+//                .addLineItem(
+//                        SessionCreateParams.LineItem.builder()
+//                                .setQuantity(1L)
+//                                .setPriceData(
+//                                        SessionCreateParams.LineItem.PriceData.builder()
+//                                                .setCurrency("usd")
+//                                                .setUnitAmount(order.getTotalPrice() * 100)
+//                                                .setProductData(
+//                                                        SessionCreateParams.LineItem.PriceData.ProductData.builder()
+//                                                                .setName("Test")
+//                                                                .build()
+//                                                )
+//                                                .build()
+//                                )
+//                                .build()
+//                )
+//                .build();
+//
+//        Session session = Session.create(params);
+//
+//        PaymentResponse paymentResponse = new PaymentResponse();
+//        paymentResponse.setPayment_url(session.getUrl());
+//
+//        return paymentResponse;
+//    }
+//}
