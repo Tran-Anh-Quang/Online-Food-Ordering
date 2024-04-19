9a4fa5fb0b56156230e4bc5514bc91b62d91149a
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-04-19 18:08:21 +0700

    chore: test integrate sonarqube

--- src/main/java/com/quangta/service/PaymentService.java
+++ src/main/java/com/quangta/service/PaymentService.java
@@ -1,9 +1,9 @@
-package com.quangta.service;
-
-import com.quangta.entity.Order;
-import com.quangta.payload.response.PaymentResponse;
-import com.stripe.exception.StripeException;
-
-public interface PaymentService {
-    public PaymentResponse createPaymentLink(Order order) throws StripeException;
-}
+//package com.quangta.service;
+//
+//import com.quangta.entity.Order;
+//import com.quangta.payload.response.PaymentResponse;
+//import com.stripe.exception.StripeException;
+//
+//public interface PaymentService {
+//    public PaymentResponse createPaymentLink(Order order) throws StripeException;
+//}
